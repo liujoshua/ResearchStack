76c91102f14e1f634551d8fdcb97c9ab02d0e240
Joshua Liu <joshua.liu@sagebase.org>
2017-01-05 10:42:55 -0800

    Override methods for DataResponse, User

--- skin/src/main/java/org/researchstack/skin/DataResponse.java
+++ skin/src/main/java/org/researchstack/skin/DataResponse.java
@@ -2,6 +2,7 @@ package org.researchstack.skin;
 
 public class DataResponse
 {
+
     private String message;
 
     private boolean success;
@@ -36,4 +37,30 @@ public class DataResponse
         this.success = success;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DataResponse that = (DataResponse) o;
+
+        if (success != that.success) return false;
+        return message != null ? message.equals(that.message) : that.message == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = message != null ? message.hashCode() : 0;
+        result = 31 * result + (success ? 1 : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "DataResponse{" +
+                "message='" + message + '\'' +
+                ", success=" + success +
+                '}';
+    }
 }
--- skin/src/main/java/org/researchstack/skin/model/User.java
+++ skin/src/main/java/org/researchstack/skin/model/User.java
@@ -47,4 +47,33 @@ public class User implements Serializable
         this.birthDate = birthDate;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (name != null ? !name.equals(user.name) : user.name != null) return false;
+        if (email != null ? !email.equals(user.email) : user.email != null) return false;
+        return birthDate != null ? birthDate.equals(user.birthDate) : user.birthDate == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (birthDate != null ? birthDate.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate='" + birthDate + '\'' +
+                '}';
+    }
 }
